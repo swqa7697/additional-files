a90c08c40c17eed1896621e295d9639fa2a863b1
swqa7697 <swqa7697@gmail.com>
2024-04-25 07:05:38 -0700

    Add comments

--- cs6650-airline-aggregation/constants.mjs
+++ cs6650-airline-aggregation/constants.mjs
@@ -1,6 +1,9 @@
 export const routes = {
-    'Test Airline 1': '13.57.207.97',
-    'Test Airline 2': '54.219.68.8',
+    // List Public IP address of the load balancer of each airline's backend API servers
+    // Format should be 'airline name': 'ip address'
+    // Use the airline name that you have indicated in the config.json files of backend servers
+    'Airline 1': '01.02.03.04',
+    'Airline 2': '05.06.07.08',
 };
 
 export const port = 80;
